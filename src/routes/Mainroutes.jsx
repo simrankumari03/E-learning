beb01c1b3fdfdf9906bf2a2073860adb71aa583f
simrankumari03 <simransah839@gamil.com>
2025-07-27 00:35:11 +0530

    Fix import case for Course and Contact components

--- src/routes/Mainroutes.jsx
+++ src/routes/Mainroutes.jsx
@@ -5,8 +5,8 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
 import About from "../pages/About";
-import Course from "../pages/course"; 
-import Contact from "../pages/contact";
+import Course from "../pages/Course"; 
+import Contact from "../pages/Contact";
 
 const MainRoutes = () => {
   return (
